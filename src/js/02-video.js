830fc22ee3b232a2eeb131d21c54c119de862197
Anna Maria Jarzembowska <jarzembowska.anma@gmail.com>
2023-04-15 18:56:16 +0200

    commit unit 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  console.log(data.seconds);
+};
+
+// zapis lub aktualizacja w lokalnym magazynie istniejącego czasu video
+// setItem(key, value)
+const savePlayerTime = data => {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(savePlayerTime, 1000));
+player.on('ended', () => {
+  localStorage.removeItem('videoplayer-current-time');
+  player.unload();
+});
+
+// oczyt zapisanego czasu video z lokalnego magazynu
+const getLocalTime = () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime === 0) return 0;
+
+  const parsedTime = JSON.parse(savedTime);
+  return Number(parsedTime);
+};
+const resumePlayerOnLastTime = () => {
+  const lastPlayedTime = getLocalTime();
+  player.setCurrentTime(lastPlayedTime);
+};
+
+resumePlayerOnLastTime();
